f91d63268f6c0a19778753331a38a6c89d11b87e
AdamRiedel <riedel.a@mail.de>
2026-03-05 08:56:54 +0100

    Add status in-planung and gesamt with labels and icons

--- JobPilot/frontend/src/utils/constants.js
+++ JobPilot/frontend/src/utils/constants.js
@@ -1,27 +1,33 @@
 export const API_URL = "http://localhost:3001/api";
 
 export const STATUS = {
+  IN_PLANUNG: "in_planung",
   BEWORBEN: "beworben",
   STUFE_WEITER: "stufe_weiter",
   ANGENOMMEN: "angenommen",
   ABGELEHNT: "abgelehnt",
   KEINE_ANTWORT: "keine_antwort",
+  GESAMT: "gesamt",
 };
 
 export const STATUS_LABELS = {
+  [STATUS.IN_PLANUNG]: "In Planung",
   [STATUS.BEWORBEN]: "Beworben",
   [STATUS.STUFE_WEITER]: "Stufe weiter",
   [STATUS.ANGENOMMEN]: "Angenommen",
   [STATUS.ABGELEHNT]: "Abgelehnt",
   [STATUS.KEINE_ANTWORT]: "Keine Antwort",
+  [STATUS.GESAMT]: "Gesamt",
 };
 
 export const STATUS_ICONS = {
+  [STATUS.IN_PLANUNG]: "/plan.svg",
   [STATUS.BEWORBEN]: "/paperplane-applied.svg",
   [STATUS.STUFE_WEITER]: "/arrow-step-further.svg",
   [STATUS.ANGENOMMEN]: "/check-circle-accepted.svg",
   [STATUS.ABGELEHNT]: "/cross-circle-denied.svg",
   [STATUS.KEINE_ANTWORT]: "/clock-no-answer.svg",
+  [STATUS.GESAMT]: "/trending.svg",
 };
 
 export const BEWERBUNGSARTEN = [
