4368f0b4d73ca4a6af5cda26475220fe7803375b
adam-cmyk <riedel.a@mail.de>
2026-06-05 22:43:43 +0200

    Delete unuesed useTranslations in the app.jsx file

--- JobPilot/frontend/src/App.jsx
+++ JobPilot/frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { useTranslation } from "react-i18next";
 import "./App.css";
 import Header from "./components/Header/header";
 import StatCards from "./components/StatCards/StatCards";
@@ -17,7 +16,6 @@ import {
 } from "./services/api";
 
 function App() {
-  const { t } = useTranslation();
   const [bewerbungen, setBewerbungen] = useState([]);
   const [stats, setStats] = useState({
     beworben: 0,
